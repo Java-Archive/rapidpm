43486d05ecbcd2f61c37e44643deb7d22fb49bf6
Sven Ruppert <sven.ruppert@rapidpm.org>
2012-08-24 23:48:52 +0200

    RPM-23 : Constants - cleaning

--- base/src/org/rapidpm/Constants.java
+++ base/src/org/rapidpm/Constants.java
@@ -40,14 +40,6 @@ public class Constants {
     }
 
 
-    /**
-     * For Development
-     */
-    public static final boolean NO_INTERNET = true;
-    public static final String TAPESTRY_PRODUCTION_MODE = "false";
-
-    public static final int TITEL_LENGTH = 80;
-    public static final String TITLE = "title";
     public static final String URL = "url";
 
     public static final String PATH_SEP = "/";
@@ -60,74 +52,10 @@ public class Constants {
     public static final String FTP = DEFAULT_PROTOCOLS[2] + PROTOCOL_IDENTIFIER;
     public static final String FILE = DEFAULT_PROTOCOLS[3] + PROTOCOL_IDENTIFIER;
     public static final String HDFS = DEFAULT_PROTOCOLS[4] + PROTOCOL_IDENTIFIER;
-    //public static final String FTP = "ftp://";
-    //public static final String FILE = "file://";
-    //public static final String HDFS = "hdfs://";
 
-    public static final String DEFAULT_CHARSET = "ISO-8859-1";
-
-    public static final String URL_BLACKLIST_TXT = "url-blacklist.txt";
-
-    //    public static final String DATA_BASE_DIR = '.' + PATH_SEP + "data";
     public static final String DATA_BASE_DIR = dataPath;
 
 
-    public static final String DATA_WEBAPP_DB = DATA_BASE_DIR + PATH_SEP + "testdb.db";
-
-
-    public static final String CONF_BASE_DIR = '.' + PATH_SEP + "conf";
-
-    /**
-     * Hier werden die Konstanten die im Index als Attributfeld verwendet werden definiert.
-     */
-    public static final String IDX_CHARSET = "charset";
-    public static final String IDX_CLASSIFIED = "classified";
-    public static final String IDX_CLUSTER = "cluster";
-    public static final String IDX_COMPANY = "company";
-    //    public static final String IDX_COMPANY_ID = "companyID";
-    public static final String IDX_CONTENT = "content";
-    public static final String IDX_CONTENT_PACKED = "content_packed";
-    public static final String IDX_DOMAIN = "domain";
-    public static final String IDX_HPTDOMAIN = "hptdomain";
-    public static final String IDX_DATE = "date";
-    public static final String IDX_INDEXED = "indexed";
-    public static final String IDX_MODIFIED = "modified";
-    public static final String IDX_SOURCE = "source";
-    public static final String IDX_URL = "url";
-    public static final String IDX_KEYWORDS = "keywords";
-    public static final String IDX_BOOST = "boost";
-    public static final String IDX_USER = "user";
-    public static final String IDX_MANDANTEGRUPPE = "mandantengruppe";
-    public static final String IDX_SUCHMODUL = "suchmodul";
-    public static final String IDX_SUCHMODULFILTER = "suchmodulfilter";
-    public static final String IDX_SUCHMODULFILTEROIDS = "filterOIDS";
-    public static final String IDX_SUBJECT = "subject";
-    public static final String IDX_SCORE = "score";
-    public static final String IDX_BERECHTIGUNG = "berechtigung";
-    public static final String IDX_TITLE = "title";
-    public static final String IDX_LINK = "link";
-    public static final String IDX_LINKS = "links";
-    public static final String IDX_OID = "oid";
-    public static final String IDX_ID = "id";
-    public static final String IDX_IDXVERSION = "idxversion";
-    public static final String IDX_DESCRIPTION = "description";
-    public static final String IDX_AUTHOR = "author";
-    public static final String IDX_ROBOTS = "robots";
-    public static final String IDX_GEO_REGION = "geo.region";
-    public static final String IDX_GEO_PLACENAME = "geo.placename";
-    public static final String IDX_GEO_POSITION = "geo.position";
-    public static final String IDX_GEO_LAT = "geo:lat";
-    public static final String IDX_GOOGLE_SITE_VERIFICATION = "google-site-verification";
-    public static final String IDX_PAGE_TOPIC = "page-topic";
-    public static final String IDX_PAGE_TYPE = "page-type";
-    public static final String IDX_PAGE_TITLE = "page-title";
-    public static final String IDX_PUBLISHER = "publisher";
-    public static final String IDX_WARC_IP_ADRESS = "warc-ip-address";
-    public static final String IDX_REVISIT_AFTER = "revisit-after";
-    public static final String IDX_VERSION = "idxVersion";
-
-
-    public static final String INDEX = "index";
 
 
     public static final String MAIL = "mail";
@@ -161,10 +89,8 @@ public class Constants {
 
     public static final String PERSISTENCE_UNIT_NAME_TEST = "junitORMJDBC";
 
-    public static final int MAX_DATAVOLUME_FOR_READING = 20 * 1024 * 1024;
-
-    public static final String HTTP_SOLR_CORE0 = "http://prod.neoscio.de:8983/solr/core0";
-    public static final String HTTP_SOLR_CORE1 = "http://prod.neoscio.de:8983/solr/core1";
+//    public static final String HTTP_SOLR_CORE0 = "http://prod.neoscio.de:8983/solr/core0";
+//    public static final String HTTP_SOLR_CORE1 = "http://prod.neoscio.de:8983/solr/core1";
 
     //    public static final String ORM_VIEW_TMP_DIR = "D:\\opt\\jboss-as-7.1.1.Final\\standalone\\tmp";
     public static final String ORM_VIEW_TMP_DIR = "/opt/jboss/7.1.1/standalone/tmp";
