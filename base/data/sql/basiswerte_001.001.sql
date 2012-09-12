094d4643956c96731df1b0bcd55a9db7a668c8fb
Sven Ruppert <sven.ruppert@rapidpm.org>
2012-09-12 05:54:05 +0200

    RPM-39 added the pk insert statements

--- base/data/sql/basiswerte_001.001.sql
+++ base/data/sql/basiswerte_001.001.sql
@@ -1,15 +1,23 @@
 INSERT INTO
         mandantengruppe(id, mandantengruppe) VALUES (1, 'RapidPM');
+
+INSERT INTO pk_gen(gen_key, gen_value) VALUES ('Mandantengruppe_id', 1);
+
 INSERT INTO
         benutzergruppe(id, gruppenname)VALUES (1, 'User');
 INSERT INTO
         benutzergruppe(id, gruppenname)VALUES (2, 'Anonymous');
 INSERT INTO
         benutzergruppe(id, gruppenname)VALUES (3, 'Admin');
+
+INSERT INTO pk_gen(gen_key, gen_value) VALUES ('BenutzerGruppe_id', 3);
+
 INSERT INTO
         benutzerwebapplikation(id, webappname)VALUES (1, 'RapidPM');
 INSERT INTO
         benutzerwebapplikation(id, webappname)VALUES (2, 'RapidPMBeta');
+
+INSERT INTO pk_gen(gen_key, gen_value) VALUES ('BenutzerWebapplikation_id', 2);
 
 INSERT INTO
         benutzer(
@@ -20,6 +28,7 @@ INSERT INTO
                 '2012-09-11',
                 '2013-09-11', 1, 1,1);
 
+INSERT INTO pk_gen(gen_key, gen_value) VALUES ('Benutzer_id', 1);
 
 INSERT INTO
         ressourcegroup(id, name, costperhour)
@@ -52,3 +61,5 @@ INSERT INTO
 INSERT INTO
         ressourcegroup(id, name, costperhour)
         VALUES (6, 'backoffice', 0.0);
+
+INSERT INTO pk_gen(gen_key, gen_value) VALUES ('RessourceGroup_id', 6);
