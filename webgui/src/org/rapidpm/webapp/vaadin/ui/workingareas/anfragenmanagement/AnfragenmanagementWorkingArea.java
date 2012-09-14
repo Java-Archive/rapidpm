cddb3cf1de2e4b7c8b36966bd77909f501d89430
Marco Ebbinghaus <marco.ebbinghaus@rapidpm.org>
2012-09-14 13:11:17 +0200

    RPM-61: fixed NPE after click on request management in menubar

--- webgui/src/org/rapidpm/webapp/vaadin/ui/workingareas/anfragenmanagement/AnfragenmanagementWorkingArea.java
+++ webgui/src/org/rapidpm/webapp/vaadin/ui/workingareas/anfragenmanagement/AnfragenmanagementWorkingArea.java
@@ -3,11 +3,7 @@ package org.rapidpm.webapp.vaadin.ui.workingareas.anfragenmanagement;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
-import org.rapidpm.persistence.DaoFactoryBean;
 import org.rapidpm.persistence.prj.bewegungsdaten.anfragen.Projektanfrage;
-import org.rapidpm.persistence.prj.bewegungsdaten.anfragen.ProjektanfrageDAO;
-
-import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,12 +15,12 @@ import java.util.List;
 public class AnfragenmanagementWorkingArea extends VerticalLayout {
 
     public AnfragenmanagementWorkingArea() {
-        final DaoFactoryBean daoFactoryBean = null; // TODO AnfragenmanagementWorkingAreaBean
-        final ProjektanfrageDAO projektanfrageDAO = daoFactoryBean.getProjektanfrageDAO();
+        //final DaoFactoryBean daoFactoryBean = null; // TODO AnfragenmanagementWorkingAreaBean
+        //final ProjektanfrageDAO projektanfrageDAO = daoFactoryBean.getProjektanfrageDAO();
 
         final BeanItemContainer<Projektanfrage> dataSource = new BeanItemContainer<>(Projektanfrage.class);
-        final List<Projektanfrage> projektanfrageList = projektanfrageDAO.loadAllEntities();
-        dataSource.addAll(projektanfrageList);
+        //final List<Projektanfrage> projektanfrageList = projektanfrageDAO.loadAllEntities();
+        //dataSource.addAll(projektanfrageList);
 
         final Table table = new Table("Projektanfragen", dataSource);
         table.setVisibleColumns(ProjektanfrageUI.VISIBLE_COLUMNS);
