d4a47bfbfa00f29231a706c5a389d9fc05c7a73f
Sven Ruppert <sven.ruppert@rapidpm.org>
2012-10-12 23:27:00 +0200

    RPM-220 : Projekt verbinden mit der Spec

--- base/src/org/rapidpm/persistence/prj/projectmanagement/planning/PlannedProject.java
+++ base/src/org/rapidpm/persistence/prj/projectmanagement/planning/PlannedProject.java
@@ -1,5 +1,6 @@
 package org.rapidpm.persistence.prj.projectmanagement.planning;
 
+import org.rapidpm.persistence.prj.book.Buch;
 import org.rapidpm.persistence.prj.projectmanagement.planning.finance.PlannedOffer;
 import org.rapidpm.persistence.prj.projectmanagement.planning.management.travel.PlannedTravel;
 import org.rapidpm.persistence.system.security.Benutzer;
@@ -57,6 +58,13 @@ public class PlannedProject {
     @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
     private List<PlannedOffer> plannedOfferList;
 
+
+    @OneToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    private Buch specification;
+
+
+
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
@@ -77,18 +85,74 @@ public class PlannedProject {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof PlannedProject)) return false;
 
         PlannedProject that = (PlannedProject) o;
 
+        if (active != that.active) return false;
+        if (fakturierbar != that.fakturierbar) return false;
+        if (creator != null ? !creator.equals(that.creator) : that.creator != null) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (info != null ? !info.equals(that.info) : that.info != null) return false;
+        if (mandantengruppe != null ? !mandantengruppe.equals(that.mandantengruppe) : that.mandantengruppe != null)
+            return false;
+        if (plannedOfferList != null ? !plannedOfferList.equals(that.plannedOfferList) : that.plannedOfferList != null)
+            return false;
+        if (plannedProjectName != null ? !plannedProjectName.equals(that.plannedProjectName) : that.plannedProjectName != null)
+            return false;
+        if (plannedTravelList != null ? !plannedTravelList.equals(that.plannedTravelList) : that.plannedTravelList != null)
+            return false;
+        if (planningUnits != null ? !planningUnits.equals(that.planningUnits) : that.planningUnits != null)
+            return false;
         if (projektName != null ? !projektName.equals(that.projektName) : that.projektName != null) return false;
+        if (responsiblePerson != null ? !responsiblePerson.equals(that.responsiblePerson) : that.responsiblePerson != null)
+            return false;
+        if (specification != null ? !specification.equals(that.specification) : that.specification != null)
+            return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return projektName != null ? projektName.hashCode() : 0;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (mandantengruppe != null ? mandantengruppe.hashCode() : 0);
+        result = 31 * result + (plannedProjectName != null ? plannedProjectName.hashCode() : 0);
+        result = 31 * result + (projektName != null ? projektName.hashCode() : 0);
+        result = 31 * result + (planningUnits != null ? planningUnits.hashCode() : 0);
+        result = 31 * result + (active ? 1 : 0);
+        result = 31 * result + (fakturierbar ? 1 : 0);
+        result = 31 * result + (info != null ? info.hashCode() : 0);
+        result = 31 * result + (creator != null ? creator.hashCode() : 0);
+        result = 31 * result + (responsiblePerson != null ? responsiblePerson.hashCode() : 0);
+        result = 31 * result + (plannedTravelList != null ? plannedTravelList.hashCode() : 0);
+        result = 31 * result + (plannedOfferList != null ? plannedOfferList.hashCode() : 0);
+        result = 31 * result + (specification != null ? specification.hashCode() : 0);
+        return result;
+    }
+
+    public List<PlannedTravel> getPlannedTravelList() {
+        return plannedTravelList;
+    }
+
+    public void setPlannedTravelList(List<PlannedTravel> plannedTravelList) {
+        this.plannedTravelList = plannedTravelList;
+    }
+
+    public List<PlannedOffer> getPlannedOfferList() {
+        return plannedOfferList;
+    }
+
+    public void setPlannedOfferList(List<PlannedOffer> plannedOfferList) {
+        this.plannedOfferList = plannedOfferList;
+    }
+
+    public Buch getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(Buch specification) {
+        this.specification = specification;
     }
 
     public boolean isActive() {
