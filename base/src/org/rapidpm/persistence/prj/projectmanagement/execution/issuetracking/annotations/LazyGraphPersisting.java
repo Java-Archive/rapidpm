884cff37812e8cd0ad301494d8006e197d560c66
Alvin Schiller <alvin.schiller@rapidpm.org>
2012-11-14 10:07:24 +0100

    RPM-209: Annotation wieder hinzugefügt

---
+++ base/src/org/rapidpm/persistence/prj/projectmanagement/execution/issuetracking/annotations/LazyGraphPersisting.java
@@ -0,0 +1,18 @@
+package org.rapidpm.persistence.prj.projectmanagement.execution.issuetracking.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alvin
+ * Date: 14.11.12
+ * Time: 10:06
+ * To change this template use File | Settings | File Templates.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LazyGraphPersisting {
+}
