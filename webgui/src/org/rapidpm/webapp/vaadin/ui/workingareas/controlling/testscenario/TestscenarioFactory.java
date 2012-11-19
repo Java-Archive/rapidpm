282f6f2dbb1ab31e50a3145690c2b394277d191f
Daniel Macdonald <daniel.macdonald@rapidpm.org>
2012-11-19 16:34:13 +0100

    RPM-115 Branch erstellt, RPM-142 (UI Componenten) eingemerget,  initialer commit

---
+++ webgui/src/org/rapidpm/webapp/vaadin/ui/workingareas/controlling/testscenario/TestscenarioFactory.java
@@ -0,0 +1,11 @@
+package org.rapidpm.webapp.vaadin.ui.workingareas.controlling.testscenario;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: donnie
+ * Date: 19.11.12
+ * Time: 16:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestscenarioFactory {
+}
