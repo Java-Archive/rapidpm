6e68b981f1913d291bb6a24a92a29244968253c8
Marco Ebbinghaus <marco.ebbinghaus@rapidpm.org>
2012-11-21 14:03:14 +0100

    RPM-216: Fehlersicherheit beim Anlegen neuer Benutzer fertig

---
+++ webgui/src/org/rapidpm/webapp/vaadin/ui/workingareas/stammdaten/benutzer/exceptions/UsernameAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package org.rapidpm.webapp.vaadin.ui.workingareas.stammdaten.benutzer.exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marco
+ * Date: 21.11.12
+ * Time: 13:09
+ * This is part of the RapidPM - www.rapidpm.org project. please contact chef@sven-ruppert.de
+ */
+public class UsernameAlreadyExistsException extends AlreadyExistsException {
+}
