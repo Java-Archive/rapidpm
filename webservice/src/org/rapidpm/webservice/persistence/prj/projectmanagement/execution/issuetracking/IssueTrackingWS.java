7940b01295e7ee333d82a400e25ccacfa156fe2e
Alexander Vos <alexander.vos@rapidpm.org>
2012-12-13 16:57:47 +0100

    RPM-298 IssueTrackingWS erweitert

--- webservice/src/org/rapidpm/webservice/persistence/prj/projectmanagement/execution/issuetracking/IssueTrackingWS.java
+++ webservice/src/org/rapidpm/webservice/persistence/prj/projectmanagement/execution/issuetracking/IssueTrackingWS.java
@@ -11,6 +11,7 @@ import org.rapidpm.webservice.persistence.prj.projectmanagement.execution.issuet
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,6 +23,7 @@ import java.util.List;
 public class IssueTrackingWS {
     private static final Logger logger = Logger.getLogger(IssueTrackingWS.class);
 
+    private final DaoFactory daoFactory = DaoFactorySingelton.getInstance();
     private IssueBaseDAO issueBaseDAO;
 
     private final EntityMapper<IssueBase, FlatIssueBase> issueBaseMapper = new EntityMapper<IssueBase, FlatIssueBase>(IssueBase.class, FlatIssueBase.class) {
@@ -32,9 +34,58 @@ public class IssueTrackingWS {
     };
 
     @WebMethod
+    public FlatIssueBase findIssueById(@WebParam(name = "projectId") final Long projectId,
+                                       @WebParam(name = "id") final Long id) {
+        issueBaseMapper.checkPermission(EntityMapper.PERMISSION_SELECT);
+        issueBaseDAO = daoFactory.getIssueBaseDAO(projectId);
+        final IssueBase issueBase = issueBaseDAO.findByID(id);
+        return issueBaseMapper.toFlatEntity(issueBase);
+    }
+
+    @WebMethod
+    public List<FlatIssueBase> findIssuesByIdList(@WebParam(name = "projectId") final Long projectId,
+                                                  @WebParam(name = "idList") final List<Long> idList) {
+        issueBaseMapper.checkPermission(EntityMapper.PERMISSION_SELECT);
+        issueBaseDAO = daoFactory.getIssueBaseDAO(projectId);
+        final List<IssueBase> issueBaseList = new ArrayList<>();
+        for (final Long id : idList) {
+            final IssueBase issueBase = issueBaseDAO.findByID(id);
+            issueBaseList.add(issueBase);
+        }
+        return issueBaseMapper.toFlatEntityList(issueBaseList);
+    }
+
+    @WebMethod
+    public void saveIssue(@WebParam(name = "entity") final FlatIssueBase flatIssueBase) {
+        issueBaseMapper.checkPermission(EntityMapper.PERMISSION_UPDATE);
+        final Long projectId = flatIssueBase.getProjectId();
+        issueBaseDAO = daoFactory.getIssueBaseDAO(projectId);
+        final IssueBase issueBase = issueBaseMapper.toEntity(flatIssueBase);
+        issueBaseDAO.persist(issueBase);
+    }
+
+    @WebMethod
+    public void deleteIssue(@WebParam(name = "projectId") final Long projectId,
+                            @WebParam(name = "id") final Long id) {
+        issueBaseMapper.checkPermission(EntityMapper.PERMISSION_DELETE);
+        issueBaseDAO = daoFactory.getIssueBaseDAO(projectId);
+        final IssueBase issueBase = issueBaseDAO.findByID(id);
+        issueBaseDAO.delete(issueBase);
+    }
+
+    @WebMethod
+    public List<FlatIssueBase> getSubIssues(@WebParam(name = "projectId") final Long projectId,
+                                            @WebParam(name = "issueId") final Long issueId) {
+        issueBaseMapper.checkPermission(EntityMapper.PERMISSION_SELECT);
+        issueBaseDAO = daoFactory.getIssueBaseDAO(projectId);
+        final IssueBase issueBase = issueBaseDAO.findByID(issueId);
+        final List<IssueBase> subIssues = issueBaseDAO.getSubIssuesOf(issueBase);
+        return issueBaseMapper.toFlatEntityList(subIssues);
+    }
+
+    @WebMethod
     public List<FlatIssueBase> getTopLevelIssues(@WebParam(name = "projectId") final Long projectId) {
         issueBaseMapper.checkPermission(EntityMapper.PERMISSION_SELECT);
-        final DaoFactory daoFactory = DaoFactorySingelton.getInstance();
         issueBaseDAO = daoFactory.getIssueBaseDAO(projectId);
         final List<IssueBase> issueBaseList = issueBaseDAO.loadTopLevelEntities();
         return issueBaseMapper.toFlatEntityList(issueBaseList);
