d1568b7cde601cc4cd42e360087764c12b0e7dcf
Sven Ruppert <sven.ruppert@rapidpm.org>
2013-01-29 21:36:28 +0100

    added project files

--- base/data/sql/basiswerte_noprojects.sql
+++ base/data/sql/basiswerte_noprojects.sql
@@ -26,3 +26,14 @@ INSERT INTO
         VALUES (1, true , 'administrator@rapidpm.org', 0, false , '2012-11-22', 'administrator', 'geheim',
                 '2012-11-22',
                 '2013-11-22', 1, 1,1);
+
+INSERT INTO
+        benutzer(
+                id, active, email, failedlogins, hidden, lastlogin, login, passwd,
+                validfrom, validuntil, benutzergruppe_id, benutzerwebapplikation_id,
+                mandantengruppe_id)
+        VALUES (2, true , 'sven.ruppert@rapidpm.org', 0, false , '2012-11-22', 'sven.ruppert', 'geheim',
+                '2012-11-22',
+                '2013-11-22', 1, 1,1);
+
+INSERT INTO pk_gen(gen_key, gen_value) VALUES ('Benutzer_id', 1);
