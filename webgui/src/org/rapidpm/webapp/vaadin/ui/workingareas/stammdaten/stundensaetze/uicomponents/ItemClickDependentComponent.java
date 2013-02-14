ee4a00c902c2c75b3f4196c36dac9879c759043f
Sven Ruppert <sven.ruppert@rapidpm.org>
2013-02-14 15:53:06 +0100

    change to maven

--- webgui/src/org/rapidpm/webapp/vaadin/ui/workingareas/stammdaten/stundensaetze/uicomponents/ItemClickDependentComponent.java
+++
@@ -1,7 +0,0 @@
-package org.rapidpm.webapp.vaadin.ui.workingareas.stammdaten.stundensaetze.uicomponents;
-
-public interface ItemClickDependentComponent {
-    public void getTheState(boolean state);
-
-    public void setItemId(Object itemId);
-}
